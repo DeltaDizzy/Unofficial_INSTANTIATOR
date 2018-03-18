e934911bbacdc6db6a99d3f5ad1b973391ff19a3
DeltaDizzy <EJBraswell@gmail.com>
2018-03-18 14:35:19 -0500

    save and stuff(tm)

--- INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/LocalSound.cs
+++ INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/LocalSound.cs
@@ -7,8 +7,8 @@ namespace INSTANTIATOR
        public string name;
        public string body;
        public string filePath;
-       public int outerSoundRadius;
-       public int innerSoundRadius;
+       public int outerRadius;
+       public int innerRadius;
        public bool loop;
        public LocalSound(string n, string b, string fp, string osr, string isr, string l)
        {
@@ -24,10 +24,16 @@ namespace INSTANTIATOR
        {
           CelestialBody targetbody = FlightGlobals.GetBodyByName(body);
           Debug.Log(INSTANTIATOR.Log("Initializing LocalSound: " + name + " around body " + body + "."));
-          GameObject soundSource = GameObject.AddComponent(AudioSource);
+          GameObject soundSource = GameObject.AddComponent<AudioSource>();
         
-          soundSource.transform.SetParent(targetbody.scaledbody.transform);
+          soundSource.transform.SetParent(targetbody.scaledBody.transform);
           soundSource.transform.localPosition = Vector3.zero;
+          soundSource.name = name; 
+           
+          if(FlightGlobals.activeVessel.altitude >= innerRadius && FlightGlobals.activeVessel.altitude <= outerRadius)
+          {
+                
+          }
           
        }
     }
