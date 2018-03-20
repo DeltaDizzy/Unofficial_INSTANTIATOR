0eea12f2d0b4d2cacab83a6b82bef9e306465242
DeltaDizzy <EJBraswell@gmail.com>
2018-03-19 19:21:18 -0500

    Update MusicUtils.cs

--- INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/MusicUtils.cs
+++ INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/MusicUtils.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
 
+namespace INSTANTIATOR
+{
+  public class MusicUtils : MonoBehaviour
+  {
+    private static string _musicPath;
+        public static string MusicPath
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_musicPath))
+                {
+                    _musicPath = Directory.GetParent(KSPUtil.ApplicationRootPath).FullName.ToString().Replace("\\", "/") + "/Music";
+                    INSTANTIATOR.Log("Music path is: " + _musicPath);
+                }
+                return _musicPath;
+            }
+        }
+  }
+}
