6bd672e30bf6e7f2dc51e0ad65526645f521376d
DeltaDizzy <EJBraswell@gmail.com>
2018-03-19 19:25:59 -0500

    Update and rename MusicUtils.cs to MusicLoader.cs

---
+++ INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/MusicLoader.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace INSTANTIATOR
+{
+  public class MusicUtils : MonoBehaviour
+  {
+        private static string _musicPath;
+        public static string MusicPath
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_musicPath))
+                {
+                    _musicPath = Directory.GetParent(KSPUtil.ApplicationRootPath).FullName.ToString().Replace("\\", "/") + "/Music";
+                    INSTANTIATOR.Log("Music path is: " + _musicPath);
+                }
+                return _musicPath;
+            }
+        }
+        
+        public static AudioClip GetAudioClip(string name)
+        {
+            // Ensure the Music directory exists.
+            Directory.CreateDirectory(MusicPath);
+
+            foreach (string file in Directory.GetFiles(MusicPath, "*", SearchOption.AllDirectories))
+            {
+                if (name.Equals(Path.GetFileNameWithoutExtension(file)))
+                {
+                    string ext = Path.GetExtension(file);
+                    INSTANTIATOR.Log("Found " + name + ", with extension " + ext);
+                    switch (ext.ToUpperInvariant())
+                    {
+                        case ".WAV":
+                        case ".OGG":
+                            return LoadUnityAudioClip(file);
+                        default:
+                            INSTANTIATOR.Log("Unknown extension found: " + ext);
+                            break;
+                    }
+                }
+            }
+  }
+}
