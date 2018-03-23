f92bb901e3dad546a8762e936626e4c0b8bd1608
DeltaDizzy <EJBraswell@gmail.com>
2018-03-23 15:50:03 -0500

    Update AudioLoader.cs

--- INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/AudioLoader.cs
+++ INSTANTIATOR/INSTANTIATOR/INSTANTIATOR/AudioLoader.cs
@@ -1,15 +1,1 @@
-using UnityEngine;
 
-namespace INSTANTIATOR
-{
-  public class AudioLoader : MonoBehaviour
-  {
-    private string _audioPath;
-    public AudioPath { get; set; }
-    
-    public float minDistance = 1;
-    public float maxDistance = FlightGlobals.ActiveVessel.altitude + 10;
-    public bool loop = true;
-    public float volume = 1;
-  }
-}
